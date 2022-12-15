cca81dca6bf2e8dbf7a502db5eb0249fe6e44a99
andrewcargill <andycargill01@aol.com>
2022-12-15 18:11:08 +0000

    remove 'total' from power model & add landing page message

---
+++ heros_api/views.py
@@ -0,0 +1,8 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view()
+def root_route(request):
+    return Response({
+        "message": "S U P E R H E R O   A P I   I S   W O R K I N G ! "
+    })
