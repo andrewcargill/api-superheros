45d758bbcd782a88b47e5fb20e05be1ba64bf352
andrewcargill <andycargill01@aol.com>
2022-12-15 13:07:24 +0000

    create super hero profile model

--- profiles/models.py
+++ profiles/models.py
@@ -7,8 +7,8 @@ class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=255, blank=True)
-    content = models.TextField(blank=True)
+    hero_name = models.CharField(max_length=20, blank=True)
+    bio = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_hr5pcw'
     )
