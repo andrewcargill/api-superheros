3d4532d7adb47dfd531608b9d3954232d70504c6
andrewcargill <andycargill01@aol.com>
2022-12-15 15:54:32 +0000

    include functioning comment model and view

--- comment/models.py
+++ comment/models.py
@@ -1,18 +1,17 @@
 from django.db import models
+from posts.models import Post
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    caption = models.CharField(max_length=100)
-    image = models.ImageField(
-        upload_to='images/', default='default_post_kg2mvg', blank=True
-    )
-
+    
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f'{self.id} {self.caption}'
+        return self.content
