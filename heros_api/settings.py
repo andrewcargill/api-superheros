85c6550f94aa0e6b77688d01cab2f1daefc16774
andrewcargill <andycargill01@aol.com>
2022-12-16 07:17:18 +0000

    include date formating(working) and pagination(not working)

--- heros_api/settings.py
+++ heros_api/settings.py
@@ -29,8 +29,19 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+    'DATETIME_FORMAT': '%d %b %y',
 }
+
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
+print(REST_FRAMEWORK)
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
@@ -64,6 +75,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
     'django.contrib.sites',
