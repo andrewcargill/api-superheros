e3c83ea43ad6776a7cfad9b8023ba0929e409591
andrewcargill <andycargill01@aol.com>
2022-12-16 09:05:48 +0000

    correct allowed hosts

--- heros_api/settings.py
+++ heros_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://superheros.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'superheros.herokuapp.com']
 
 
 # Application definition
