c1c49b598b6cd9c5ccbd36b0109cabd8dce8a1aa
andrewcargill <andycargill01@aol.com>
2022-12-21 17:49:38 +0000

    enable post pagination

--- posts/views2.py
+++ posts/views2.py
@@ -1,26 +1,76 @@
 from django.http import Http404
-from rest_framework import generics, status, permissions
+from rest_framework import status, permissions
 from rest_framework.response import Response
-from django.views.generic import ListView
 from rest_framework.views import APIView
-from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post
 from .serializers import PostSerializer
 from heros_api.permissions import IsOwnerOrReadOnly
 
+#list of posts
+class PostList(APIView):
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
 
-class PostList(ListView):
-    """
-    List posts or create a post if logged in
-    The perform_create method associates the post with the logged in user.
-    """
-    model = Post
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
 
+    def post(self, request):
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
 
+# single post view
+class PostDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PostSerializer
 
-class PostDetail(ListView):
-    model = Post
-    # serializer_class = PostSerializer
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    # queryset = Post.all()
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    # editing posts 
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    # delete a post
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
 
